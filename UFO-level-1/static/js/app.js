66e34457a81a191eb15341b3123fe43af8a9e47c
David <davidmrtnz20@gmail.com>
2020-11-12 21:26:26 -0600

    ;

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -2,29 +2,28 @@
 var tableData = data;
 
 // YOUR CODE HERE!
-var tbody = d3.select("tbody")
+var tbody = d3.select("tbody");
 
 function displayData(data) {
     tbody.text("")
     data.forEach(function (ufo_sighting) {
-        var row = tbody.append("tr")
+        var row = tbody.append("tr");
         Object.entries(ufo_sighting).forEach(function ([key, value]) {
-            var cell = row.append("td").text(value)
-        })
+            var cell = row.append("td").text(value);
+        });
     })
 }
 
 displayData(tableData)
 
 var inputField = d3.select("#datetime");
-var button = d3.select("filter-btn")
+var button = d3.select("filter-btn");
 
 function clickSelect() {
-    //d3.event.preventDefault();
     console.log(inputField.property("value"));
     var newTable = tableData.filter(ufo_sighting =>
         ufo_sighting.datetime == inputField.property("value"))
     displayData(newTable);
 }
 
-inputField.on("change", clickSelect)
+inputField.on("change", clickSelect);
