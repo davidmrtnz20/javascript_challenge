a461253bdeaad60e6a46eba6ef4b6b3589c831fb
David <davidmrtnz20@gmail.com>
2020-11-12 21:20:36 -0600

    finished lvl 1

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -3,3 +3,28 @@ var tableData = data;
 
 // YOUR CODE HERE!
 var tbody = d3.select("tbody")
+
+function displayData(data) {
+    tbody.text("")
+    data.forEach(function (ufo_sighting) {
+        var row = tbody.append("tr")
+        Object.entries(ufo_sighting).forEach(function ([key, value]) {
+            var cell = row.append("td").text(value)
+        })
+    })
+}
+
+displayData(tableData)
+
+var inputField = d3.select("#datetime");
+var button = d3.select("filter-btn")
+
+function clickSelect() {
+    //d3.event.preventDefault();
+    console.log(inputField.property("value"));
+    var newTable = tableData.filter(ufo_sighting =>
+        ufo_sighting.datetime == inputField.property("value"))
+    displayData(newTable);
+}
+
+inputField.on("change", clickSelect)
